6fb4274c6273352210faac673e313e7f3843afd8
dudaborges <duda.pborges92@gmail.com>
2022-08-30 17:30:32 -0300

    updateJava

--- app/src/main/java/com/example/vendasdodia/TelaCadastro.java
+++ app/src/main/java/com/example/vendasdodia/TelaCadastro.java
@@ -5,13 +5,35 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class TelaCadastro extends AppCompatActivity {
+
+    private EditText etNome;
+    private EditText etEmail;
+    private EditText etSenha;
+    private Button btCadastrar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tela_cadastro);
+        etNome = findViewById(R.id.etNome);
+        etEmail = findViewById(R.id.etEmail);
+        etSenha = findViewById(R.id.etSenha);
+        btCadastrar = findViewById(R.id.btCadastrar);
+
+        btCadastrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                recuperarDados();
+            }
+        });
+    }
+
+    private void recuperarDados() {
+        
     }
 
     public void abrir_tela_login(View v){

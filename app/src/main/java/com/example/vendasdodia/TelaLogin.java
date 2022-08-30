da71de672d5fa94fcb17fb261d269e335bc42358
dudaborges <duda.pborges92@gmail.com>
2022-08-30 18:25:34 -0300

    updateJava-login

--- app/src/main/java/com/example/vendasdodia/TelaLogin.java
+++ app/src/main/java/com/example/vendasdodia/TelaLogin.java
@@ -1,5 +1,6 @@
 package com.example.vendasdodia;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -7,9 +8,14 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.vendasdodia.modelo.Usuario;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class TelaLogin extends AppCompatActivity {
 
@@ -17,11 +23,46 @@ public class TelaLogin extends AppCompatActivity {
     private EditText etSenha;
     private Button btLogar;
     private FirebaseAuth mAuth;
+    private Usuario u;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tela_login);
+        mAuth = FirebaseAuth.getInstance();
+        etEmail = findViewById(R.id.etEmail);
+        etSenha = findViewById(R.id.etSenha);
+        btLogar = findViewById(R.id.btLogar);
+
+        btLogar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                receberDados();
+                logar();
+            }
+        });
+
+    }
+
+    private void logar() {
+        mAuth.signInWithEmailAndPassword(u.getEmail(), u.getSenha())
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            startActivity(new Intent(TelaLogin.this, TelaInicio.class));
+                        } else {
+                            Toast.makeText(TelaLogin.this, "Autentificação falhou.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
+
+    private void receberDados() {
+    u.setEmail(etEmail.getText().toString());
+    u.setSenha(etSenha.getText().toString());
     }
 
     public void abrir_tela_Inicio(View v){
